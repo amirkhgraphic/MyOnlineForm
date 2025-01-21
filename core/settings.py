25978cc8d2db8d6d32620fe6309980479dd45d13
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2025-01-22 00:13:59 +0330

    fix bug: with allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
